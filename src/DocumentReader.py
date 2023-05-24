0118e90bca6a890d96cd0bdd4058f81c7680659f
Stan <stasostaszewski@gmail.com>
2023-05-24 23:35:59 +0200

    Changes to DocumentReader

--- src/DocumentReader.py
+++ src/DocumentReader.py
@@ -2,11 +2,16 @@ import numpy as np
 import pandas as pd
 import json
 
+def __shorten_version(versions):
+    return versions[-1]["created"]
 
 def __parse_json_record(record):
     record_dict = [json.loads(record)]
     record_df = pd.DataFrame(record_dict)
-    record_df.drop(["id", "authors", "submitter", "license", "journal-ref", "update_date", "comments", "report-no", "doi"], axis=1, inplace=True)
+    version = record_df["versions"].apply(lambda x: __shorten_version(x))
+    record_df.insert(8,"version", version)
+    record_df.drop(["id", "authors", "submitter", "license", "journal-ref", "update_date", "categories", "comments", "report-no", "doi", "versions"], axis=1, inplace=True)
+
     return record_df
 
 def read_n_documents(start_reading_at = 0, document_count = 5, path = 'data//arxiv-metadata-oai-snapshot.json'):
